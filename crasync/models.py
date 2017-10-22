1d180cfe155d9606a32fbf4eb26eb1201f501c20
verixx <abdurraqeeb53@gmail.com>
2017-10-22 21:41:25 +1100

    Finished up models

--- crasync/models.py
+++ crasync/models.py
@@ -16,7 +16,7 @@ class Base:
         return f'{self.name} (#{self.tag})'
        
     async def from_data(self):
-        pass
+        raise NotImplemented
 
     async def update(self):
 
@@ -28,12 +28,11 @@ class Base:
 
         return self
 
-
-class Clan(Base):
-    '''Represents a clan'''
-
-    def from_data(self, data):
-        pass
+class ClanChest:
+    def __init__(self, data):
+        self.crowns = data.get('clanChestCrowns')
+        self.percent = data.get('clanChestCrownsPercent')
+        self.required = data.get('clanChestCrownsRequired')
 
 class Arena:
     def __init__(self, data):
@@ -42,8 +41,86 @@ class Arena:
         self.number = data.get('arenaID')
         self.trophies = data.get('trophyLimit')
 
+    @property
+    def image_url(self):
+        return f"http://api.cr-api.com{self.raw_data.get('imageURL')}"
+
     def __str__(self):
         return self.data.get('arena')
+
+class Shop:
+    def __init__(self, data):
+        self.legendary = data.get('legendary')
+        self.epic = data.get('epic')
+        self.arena = data.get('arena')
+
+class Cycle:
+    def __init__(self, data):
+        self.position = data.get('position')
+        self.super_magical = data.get('superMagicalPos')
+        self.legendary = data.get('legendaryPos')
+        self.epic = data.get('epicPos')
+
+class Card:
+    def __init__(self, data):
+        self.name = data.get('name')
+        self.rarity = data.get('rarity')
+        self.level = data.get('level')
+        self.count = data.get('count')
+        self.to_upgrade = data.get('requiredForUpgrade')
+        self.id = data.get('card_id')
+        self.elixir = data.get('elixir')
+        self.type = data.get('type')
+        self.arena = data.get('arena')
+        self.description = data.get('description')
+        self.decklink = data.get('decklink')
+
+    def __repr__(self):
+        return f'<Card id={self.id}>'
+
+class Member:
+    def __init__(self, client, data):
+        self.client = client
+        self.name = data.get('name')
+        self.arena = Arena(data.get('arena'))
+        self.role = data.get('role')
+        self.role_name = data.get('roleName')
+        self.level = data.get('expLevel')
+        self.trophies = data.get('trophies')
+        self.donations = data.get('donations')
+        self.rank = data.get('currentRank')
+        self.crowns = data.get('clanChestCrowns')
+        self.tag = data.get('tag')
+
+    def __repr__(self):
+        return f'<Member tag={self.tag}>'
+
+    def get_profile(self):
+        return self.client.get_profile(self.tag)
+
+class Clan(Base):
+    '''Represents a clan'''
+
+    def from_data(self, data):
+        self.name = data.get('name')
+        self.score = data.get('score')
+        self.required_trophies = data.get('required_trophies')
+        self.donations = data.get('donations')
+        self.rank = data.get('currentRank')
+        self.description = data.get('description')
+        self.type = data.get('type')
+        self.type_name = data.get('typeName')
+        self.region = data.get('region').get('name')
+        self.clan_chest = ClanChest(data.get('clanChest'))
+        self.members = [Member(self.client, m) for m in data.get('members')]
+
+    @property
+    def badge_url(self):
+        url = self.raw_data.get('badge').get('url')
+        return f"http://api.cr-api.com{url}"
+
+    def __repr__(self):
+        return f'<Clan tag={self.tag}>'
 
 class Profile(Base):
     '''Represents a player profile.
@@ -52,21 +129,43 @@ class Profile(Base):
     def from_data(self, data):
         self.name = data.get('name')
         exp = data.get('experience')
+        stats = data.get('stats')
+        games = data.get('games')
+        clan = data.get('clan')
         self.level = exp.get('level')
         self.experience = (exp.get('xp'), exp.get('xpRequiredForLevelUp'))
         self.name_changed = data.get('nameChanged')
         self.global_rank = data.get('globalRank')
         self.current_trophies = data.get('trophies')
-        self.highest_trophies = data.get('stats').get('challengeCardsWon')
+        self.highest_trophies = stats.get('challengeCardsWon')
         self.legend_trophies = data.get('legendaryTrophies')
+        self.tournament_cards_won = stats.get('tournamentCardsWon')
+        self.challenge_cards_won = stats.get('challengeCardsWon')
+        self.favourite_card = stats.get('favoriteCard').title()
+        self.total_donations = stats.get('totalDonations')
+        self.max_wins = stats.get('challengeMaxWins')
+        self.games_played = games.get('total')
+        self.wins = games.get('wins')
+        self.losses = games.get('losses')
+        self.draws = games.get('draws')
         self.arena = Arena(data.get('arena'))
-        self.clan_tag = data.get('clan').get('tag')
-        self.clan_name = data.get('clan').get('name')
-        self.clan_role = data.get('clan').get('role')
+        self.clan_tag = clan.get('tag')
+        self.clan_name = clan.get('name')
+        self.clan_role = clan.get('role')
+        self.shop_offers = Shop(data.get('shopOffers'))
+        self.chest_cycle = Cycle(data.get('chestCycle'))
+        self.deck = [Card(c) for c in data.get('currentDeck')]
+
+    @property
+    def clan_badge_url(self):
+        url = self.raw_data.get('clan').get('badge').get('url')
+        if not url:
+            return None
+        else:
+            return f"http://api.cr-api.com{url}"
+
+    def __repr__(self):
+        return f'<Profile tag={self.tag}>'
 
     def get_clan(self):
         return self.client.get_clan(self.clan_tag)
-
-
-
-
--- tests.py
+++ tests.py
@@ -4,8 +4,9 @@ import json
 
 async def main():
     client = crasync.Client()
-    profile = await client.get_profile('CVLQ2GV8')
-    print(json.dumps(profile.raw_data, indent=4))
+    clan = await client.get_clan('29GLUPJ')
+    print(clan)
+    print(clan.description)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
