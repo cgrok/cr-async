9406b7997a657c69f09d57418465fe6f8536f59a
verixx <abdurraqeeb53@gmail.com>
2017-10-29 01:18:29 +1100

    Tweaked testing a bit.

--- examples/example.py
+++ examples/example.py
@@ -4,28 +4,26 @@ import crasync
 import asyncio
 
 async def main():
-    client = crasync.Client()
-    profile = await client.get_profile('2CYUUCC8')
+    async with crasync.Client() as client:
+        profile = await client.get_profile('2CYUUCC8')
 
-    print(profile)
-    print(profile.current_trophies)
+        print(profile)
+        print(profile.current_trophies)
 
-    await asyncio.sleep(5)
-    await profile.update() # updating the info
+        await asyncio.sleep(2)
+        await profile.update() # updating the info
 
-    clan = await profile.get_clan() # get the players full clan info
+        clan = await profile.get_clan() # get the players full clan info
 
-    print(clan)
-    print(len(clan.members))
+        print(clan)
+        print(len(clan.members))
 
-    await asyncio.sleep(5)
-    await clan.update() # updating the info
+        await asyncio.sleep(2)
+        await clan.update() # updating the info
 
-    highest = await clan.members[0].get_profile()
+        highest = await clan.members[0].get_profile()
 
-    print(highest) # top player
-
+        print(highest) # top player
   
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
-
