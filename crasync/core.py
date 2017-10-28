ee676de7a18459659494bcd88c08d7b83a8b5aa6
verixx <abdurraqeeb53@gmail.com>
2017-10-28 11:44:33 +1100

    Fixed a space?

--- crasync/core.py
+++ crasync/core.py
@@ -22,7 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-
 import aiohttp
 import asyncio
 from .models import Profile, Clan, Constants, ClanInfo
