fd7031c71887e56279980fd5bc235d75f3b0f555
verixx <abdurraqeeb53@gmail.com>
2017-10-28 14:01:52 +1100

    Updated for the next version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='crasync',
     packages=['crasync'],  # this must be the same as the name above
-    version='v1.1.1',
+    version='v1.1.2',
     description='An async wrapper for cr-api.com',
     author='verixx',
     license='MIT',
     author_email='abdurraqeeb53@gmail.com',
     url='https://github.com/grokkers/cr-async',  # use the URL to the github repo
-    download_url='https://github.com/grokkers/cr-async/archive/v1.1.1.tar.gz',  # I'll explain this in a second
+    download_url='https://github.com/grokkers/cr-async/archive/v1.1.2.tar.gz',  # I'll explain this in a second
     keywords=['clashroyale'],  # arbitrary keywords
     classifiers=[],
     install_requires=['aiohttp>=2.0.0,<2.3.0']
