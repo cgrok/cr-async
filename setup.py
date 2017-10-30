80b7e9b712a926aa414032824e55c0a979811695
verixx <abdurraqeeb53@gmail.com>
2017-10-30 22:40:13 +1100

    Fixed the package data

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ from setuptools import setup
 setup(
     name='crasync',
     packages=['crasync'],  # this must be the same as the name above
-    version='v2.0.0',
+    version='v2.0.6',
     description='An async wrapper for cr-api.com',
     author='verixx',
     license='MIT',
     author_email='abdurraqeeb53@gmail.com',
     url='https://github.com/grokkers/cr-async',  # use the URL to the github repo
-    download_url='https://github.com/grokkers/cr-async/archive/v2.0.0.tar.gz',  # I'll explain this in a second
+    download_url='https://github.com/grokkers/cr-async/archive/v2.0.4.tar.gz',  # I'll explain this in a second
     keywords=['clashroyale'],  # arbitrary keywords
     classifiers=[],
     install_requires=['aiohttp>=2.0.0,<2.3.0'],
-    package_data={'crasync':['chests.json']},
+    package_data={
+    'crasync': ['chests.json'],
+    },
     include_package_data=True
 )
